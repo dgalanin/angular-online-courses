5d145e79fd954e483b88932f9d8557f0bd7d308e
Dmitry <dmitrycrown@yandex.ru>
2022-03-28 18:23:02 +0300

    add models

---
+++ frontend/src/app/shared/models/course.model.ts
@@ -0,0 +1,11 @@
+import {Lesson} from "./lesson.model";
+
+export class Course {
+  constructor(
+    public title: string,
+    public description: string,
+    public authorName: string,
+    public lessons: Array<Lesson>
+  ) {
+  }
+}
---
+++ frontend/src/app/shared/models/user.model.ts
@@ -0,0 +1,14 @@
+import {Course} from "./course.model";
+
+export class User {
+  constructor(
+    public email: string,
+    public password: string,
+    public name: string,
+    public isTeacher: boolean,
+    public currentCourses: Array<Course> = [],
+    public achievements: Array<Course> =  [],
+    public id?: string
+  ) {
+  }
+}
