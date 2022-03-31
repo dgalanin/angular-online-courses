22c1ac9d66d92491a18062275d85dfe7f861d229
Dmitry <dmitrycrown@yandex.ru>
2022-03-31 15:36:46 +0300

    add current courses page component

---
+++ frontend/src/app/system/current-courses-page/current-courses-page.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-current-courses-page',
+  templateUrl: './current-courses-page.component.html',
+  styleUrls: ['./current-courses-page.component.scss']
+})
+export class CurrentCoursesPageComponent {
+
+  constructor() {
+  }
+
+}
