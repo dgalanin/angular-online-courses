5a5ae026a6b3d413f4f2dfe7aae16bd8e5ef0b21
Dmitry <dmitrycrown@yandex.ru>
2022-03-31 14:10:04 +0300

    add currentUser.service.ts

---
+++ frontend/src/app/shared/services/currentUser.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from "@angular/core";
+import {Store} from "@ngrx/store";
+import {User} from "../models/user.model";
+import {CurrentUserSelectors} from "../../Store/currentUser/currentUser.selectors";
+import {CurrentUserActions} from "../../Store/currentUser/currentUser.actions";
+import {Course} from "../models/course.model";
+
+@Injectable()
+export class currentUserService {
+  constructor(private store: Store) {
+  }
+
+  get(): User {
+    let user!: User;
+    this.store.select(CurrentUserSelectors.user).subscribe(u => user = u);
+    return user;
+  }
+
+  set(user: User): void {
+    this.store.dispatch(CurrentUserActions.set(user));
+  }
+
+  reSet(): void {
+    this.store.dispatch(CurrentUserActions.reSet());
+  }
+
+  joinToCourse(course: Course): void {
+    this.store.dispatch(CurrentUserActions.joinCourse(course));
+  }
+
+  leaveFromCourse(course: Course): void {
+    this.store.dispatch(CurrentUserActions.leaveCourse(course));
+  }
+
+  achieveCourse(course: Course): void {
+    this.store.dispatch(CurrentUserActions.achieveCourse(course));
+  }
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {UsersService} from "./services/users.service";
 import {CoursesService} from "./services/courses.service";
 import {AuthService} from "./services/auth.service";
+import {currentUserService} from "./services/currentUser.service";
 
 @NgModule({
   imports: [ReactiveFormsModule, FormsModule],
   exports: [ReactiveFormsModule, FormsModule],
-  providers: [UsersService, CoursesService, AuthService]
+  providers: [UsersService, CoursesService, AuthService, currentUserService]
 })
 export class SharedModule {
 
