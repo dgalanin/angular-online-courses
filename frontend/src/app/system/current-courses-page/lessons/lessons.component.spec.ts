f53f35933aba4554dd48cc166c7530e7128980a3
dgalanin <dmitrycrown@yandex.ru>
2022-04-04 20:34:07 +0300

    add tests for lessons component

--- frontend/src/app/system/current-courses-page/lessons/lessons.component.spec.ts
+++ frontend/src/app/system/current-courses-page/lessons/lessons.component.spec.ts
@@ -1,25 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {LessonsComponent} from "./lessons.component";
+import {Lesson} from "../../../shared/models/lesson.model";
+import {By} from "@angular/platform-browser";
 
-import { LessonsComponent } from './lessons.component';
 
-describe('LessonsComponent', () => {
+describe('CurrentCoursesPageComponent', () => {
   let component: LessonsComponent;
   let fixture: ComponentFixture<LessonsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LessonsComponent ]
+      declarations: [LessonsComponent],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LessonsComponent);
     component = fixture.componentInstance;
+    component.lessons = [new Lesson("lesson1", "lesson")]
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should showLessons on click', () => {
+    fixture.debugElement.query(By.css('.btn-outline-dark')).triggerEventHandler('click', null);
+    expect(component.showLessons).toEqual(true);
+  });
+
+  it('should close showLessons on double-click', () => {
+    fixture.debugElement.query(By.css('.btn-outline-dark')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('.btn-outline-dark')).triggerEventHandler('click', null);
+    expect(component.showLessons).toEqual(false);
+  });
+
+  it('should close showLessons on close-btn', () => {
+    fixture.debugElement.query(By.css('.btn-outline-dark')).triggerEventHandler('click', null);
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('.btn-close')).triggerEventHandler('click', null);
+    expect(component.showLessons).toEqual(false);
   });
 });
