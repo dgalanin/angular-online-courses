997b070cf2e78bdf460a3821c76a17d557cd62d8
Dmitry <71137816+dgalanin@users.noreply.github.com>
2022-04-05 18:54:02 +0300

    add tests
    * add tests for courses service
    
    * add tests for users service
    
    * add tests for header component
    
    * add tests for courses page component
    
    * add tests for current-courses-page component
    
    * add tests for lessons component
    
    * fix title on lessons component tests
    
    * add tests for achievements-page component
    
    * add tests for login component
    
    * add tests for registration component
    
    * fix naming in courses-page component tests
    
    * add tests for create-course-page component
    
    * add tests for app component
    
    * add tests for system component

--- frontend/src/app/auth/registration/registration.component.spec.ts
+++ frontend/src/app/auth/registration/registration.component.spec.ts
@@ -1,6 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RegistrationComponent } from './registration.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {UsersService} from "../../shared/services/users.service";
+import {Router} from "@angular/router";
+import {By} from "@angular/platform-browser";
+import {RegistrationComponent} from "./registration.component";
+
+class mockUsersService {
+
+}
+
+class mockRouter {
+
+}
+
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
@@ -8,9 +21,18 @@ describe('RegistrationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
+      declarations: [RegistrationComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [
+        {
+          provide: UsersService, useClass: mockUsersService
+        },
+        {
+          provide: Router, useClass: mockRouter
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +43,98 @@ describe('RegistrationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain 6 inputs', () => {
+    const formElem = fixture.debugElement.nativeElement.querySelector('.form');
+    const inputElems = formElem.querySelectorAll('input');
+    expect(inputElems.length).toEqual(6);
+  });
+
+  it('should set placeholders', () => {
+    const emailElem = fixture.debugElement.nativeElement.querySelector('.form').querySelectorAll('input')[0];
+    const nameElem = fixture.debugElement.nativeElement.querySelectorAll('input')[1];
+    expect(emailElem.placeholder).toEqual('pat@example.com');
+    expect(nameElem.placeholder).toEqual('Galanin Dmitry');
+  });
+
+  it('should validate fields before entering some values', () => {
+    const emailFormControl = component.form.get('email');
+    const nameFormControl = component.form.get('name');
+    const passwordFormControl = component.form.get('password');
+    const password2FormControl = component.form.get('password2');
+    const agreeFormControl = component.form.get('agree');
+    expect(emailFormControl!.errors!['required']).toBeTruthy();
+    expect(nameFormControl!.errors!['required']).toBeTruthy();
+    expect(passwordFormControl!.errors!['required']).toBeTruthy();
+    expect(password2FormControl!.errors!['required']).toBeTruthy();
+    expect(agreeFormControl!.errors!['required']).toBeTruthy();
+  });
+
+  it('should validate fields after entering some values', () => {
+    const emailElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[0];
+    const nameElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[1];
+    const passwordElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[2];
+    const password2Elem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[3];
+    const agreeElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[5];
+
+    emailElem.value = "google@gmail.com";
+    emailElem.dispatchEvent(new Event('input'));
+    nameElem.value = "Dmitry Test";
+    nameElem.dispatchEvent(new Event('input'));
+    passwordElem.value = "google123";
+    passwordElem.dispatchEvent(new Event('input'));
+    password2Elem.value = "google123";
+    password2Elem.dispatchEvent(new Event('input'));
+    agreeElem.checked = true;
+    emailElem.dispatchEvent(new Event('checked'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const emailControl = component.form.get('email');
+      const nameControl = component.form.get('name');
+      const passwordControl = component.form.get('password');
+      const password2Control = component.form.get('password2');
+      const agreeControl = component.form.get('agree');
+      expect(emailElem.value).toEqual(emailControl?.value);
+      expect(emailControl?.errors).toBeNull();
+      expect(nameElem.value).toEqual(nameControl?.value);
+      expect(nameControl?.errors).toBeNull();
+      expect(passwordElem.value).toEqual(passwordControl?.value);
+      expect(passwordControl?.errors).toBeNull();
+      expect(password2Elem.value).toEqual(password2Control?.value);
+      expect(password2Control?.errors).toBeNull();
+      expect(agreeElem.value).toEqual(agreeControl?.value);
+      expect(agreeControl?.errors).toBeNull();
+    });
+  });
+
+  it('should validate form after entering valid values', () => {
+    const emailElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[0];
+    const nameElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[1];
+    const passwordElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[2];
+    const password2Elem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[3];
+    const agreeElem: HTMLInputElement = fixture.debugElement.nativeElement.querySelectorAll('input')[5];
+
+    emailElem.value = "google@gmail.com";
+    emailElem.dispatchEvent(new Event('input'));
+    nameElem.value = "Dmitry Test";
+    nameElem.dispatchEvent(new Event('input'));
+    passwordElem.value = "google123";
+    passwordElem.dispatchEvent(new Event('input'));
+    password2Elem.value = "google123";
+    password2Elem.dispatchEvent(new Event('input'));
+    agreeElem.checked = true;
+    emailElem.dispatchEvent(new Event('checked'));
+
+    const isFormValid = component.form.valid;
+    fixture.whenStable().then(() => {
+      expect(isFormValid).toBeTruthy();
+    });
+  });
+
+  it('should call login method on submit form', () => {
+    const onClickMock = spyOn(component, 'onSubmit');
+    fixture.debugElement.query(By.css('.form')).triggerEventHandler('submit', null);
+    expect(onClickMock).toHaveBeenCalled();
   });
 });
--- frontend/src/app/system/header/header.component.spec.ts
+++ frontend/src/app/system/header/header.component.spec.ts
@@ -1,6 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {Observable, of} from "rxjs";
+import {User} from "../../shared/models/user.model";
+import {CurrentUserService} from "../../shared/services/currentUser.service";
+
+import {AuthService} from "../../shared/services/auth.service";
+import {Router} from "@angular/router";
+
+class mockCurrentUserService {
+  get(): Observable<User> {
+    const mockUser = new User("mockEmail", "mockPass", "mockName", true);
+    return of(mockUser);
+  }
+}
+
+class mockRouter {
+
+}
+
+class mockAuthService {
+
+}
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +30,21 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [],
+      declarations: [HeaderComponent],
+      providers: [
+        {
+          provide: CurrentUserService, useClass: mockCurrentUserService
+        },
+        {
+          provide: AuthService, useClass: mockAuthService
+        },
+        {
+          provide: Router, useClass: mockRouter
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +53,20 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get user from currentUserService on initialization', () => {
+    component.ngOnInit();
+    const mockUser = new User("mockEmail", "mockPass", "mockName", true);
+    expect(component.user).toEqual(mockUser);
+  });
+
+  it('should display user name in greetings div', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    const greetings_span = fixture.nativeElement.querySelector('.greetings');
+    expect(greetings_span.text).toEqual('Hello, mockName');
   });
 });
